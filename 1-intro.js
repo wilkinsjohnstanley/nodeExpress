16172e6e648225df56c9ebbf483f288d6339b816
John Wilkins <wilkinsjohnstanley@gmail.com>
2023-12-26 11:46:56 -0600

    second commit

--- 1-intro.js
+++ 1-intro.js
@@ -1,10 +1,14 @@
 // GLOBALS - NO WINDOW !!!!
 
 /*
-__dirname - path the current directory
+__dirname - gives you the path the current directory
 __filename - file name
 require - function to use modules (CommonJS)
 module - info about current module (file)
 process - info about env where the program is being executed
 
 */
+console.log(__dirname)
+setInterval(()=> {
+    console.log('hola mundo')
+},3000)
