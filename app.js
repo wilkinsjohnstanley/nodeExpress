71ec72f3265b107d9a977ce2a595003a12f1b836
John Wilkins <wilkinsjohnstanley@gmail.com>
2023-12-31 14:09:33 -0600

    system uptime and user info

---
+++ app.js
@@ -0,0 +1,8 @@
+const os = require('os');
+
+//info about current user
+const user = os.userInfo();
+console.log(user)
+
+//method returns the system uptime in seconds
+console.log(`The System Uptime is ${os.uptime()} seconds`)
