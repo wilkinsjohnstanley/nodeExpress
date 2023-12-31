04b0f90b3b2bdca80687a8ee4831784255f05e94
John Wilkins <wilkinsjohnstanley@gmail.com>
2023-12-31 14:04:20 -0600

    we moved everything in app.js to modules.js

---
+++ 3-modules.js
@@ -0,0 +1,20 @@
+// const amount = 12
+
+// if (amount<10){
+//     console.log('small number. booo!');
+// }
+// else {
+//     console.log('large number. やった！');
+// }
+// console.log(`Hello world!!`);
+
+const names = require('./4-names')
+const sayHi = require('./5-modules')
+sayHi('John')
+sayHi(names.john)
+sayHi(names.george)
+
+const data = require('./6-alternative-export-options')
+ console.log(data)
+
+ require('./7-addValues')
