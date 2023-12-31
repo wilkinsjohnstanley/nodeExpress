e817c06d74716d0cecc7d08367043489bb850538
John Wilkins <wilkinsjohnstanley@gmail.com>
2023-12-31 14:15:30 -0600

    build in modules are powerful

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ console.log(user)
 
 //method returns the system uptime in seconds
 console.log(`The System Uptime is ${os.uptime()} seconds`)
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+}
+console.log(currentOS);
