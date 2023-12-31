c870c23b8b509a8e7b5294a5a679b3e784b30da5
John Wilkins <wilkinsjohnstanley@gmail.com>
2023-12-31 14:24:17 -0600

    working with path module

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ const path = require('path');
 
 console.log(path.sep);
 
+const filePath = path.join('/content', 'subfolder', 'text.txt')
+console.log(filePath)
+
+const base = path.basename(filePath);
+console.log(base)
+//points to directory where app.js is located
+const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt');
+console.log(absolute)
+//getting an absolute path is great for when you want to run in multiple differernt environments
