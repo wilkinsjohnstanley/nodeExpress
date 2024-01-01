8a2e7a4cd94bc2aafa4b3dd33492c8b79c84d60b
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-01 10:47:44 -0600

    async error handling

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const { readFile, writeFile } = require('fs')
 
-readFile('./content/first.txt', (err, result)=>{
+readFile('./content/first.txt','utf8' ,(err, result)=>{
     if(err){
+        console.log(err)
         return
     }
-    console.log(result)
+    const first = result;
+    readFile('./content/text.txt', 'utf8', ()=>{
+if(err){
+    console.log(err)
+    return
+}
+const text = result
+writeFile(
+    './content/result-aync.txt', 
+    `hello world! Here are my thoughts: ${first}, ${text}`, (err, result)=>{
+        if(err){
+            console.log(err)
+            return;
+        }
+        console.log(result)
+    }
+    )
+    })
 })
